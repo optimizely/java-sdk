f1069635e2c710111ce8d26af77976cdadda463a
wangjoshuah <wangjoshuah@gmail.com>
2017-07-25 17:00:40 -0700

    log live variable parsing error when Gson parsing feature flags
    Signed-off-by: wangjoshuah <wangjoshuah@gmail.com>

--- core-api/src/main/java/com/optimizely/ab/config/parser/GsonHelpers.java
+++ core-api/src/main/java/com/optimizely/ab/config/parser/GsonHelpers.java
@@ -20,7 +20,9 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
+import com.optimizely.ab.bucketing.DecisionService;
 import com.optimizely.ab.config.Experiment;
 import com.optimizely.ab.config.Experiment.ExperimentStatus;
 import com.optimizely.ab.config.FeatureFlag;
@@ -28,6 +30,8 @@ import com.optimizely.ab.config.LiveVariable;
 import com.optimizely.ab.config.LiveVariableUsageInstance;
 import com.optimizely.ab.config.TrafficAllocation;
 import com.optimizely.ab.config.Variation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -37,6 +41,8 @@ import java.util.Map;
 import java.util.Set;
 
 final class GsonHelpers {
+
+    private static final Logger logger = LoggerFactory.getLogger(DecisionService.class);
 
     private static List<Variation> parseVariations(JsonArray variationJson, JsonDeserializationContext context) {
         List<Variation> variations = new ArrayList<Variation>(variationJson.size());
@@ -127,9 +133,17 @@ final class GsonHelpers {
             experimentIds.add(experimentIdObj.getAsString());
         }
 
-        Type liveVariableType = new TypeToken<List<LiveVariable>>() {}.getType();
-        List<LiveVariable> liveVariables = context.deserialize(featureFlagJson.getAsJsonArray("variables"),
-                liveVariableType);
+        List<LiveVariable> liveVariables = new ArrayList<LiveVariable>();
+        try {
+            Type liveVariableType = new TypeToken<List<LiveVariable>>() {
+            }.getType();
+            liveVariables = context.deserialize(featureFlagJson.getAsJsonArray("variables"),
+                    liveVariableType);
+        }
+        catch (JsonParseException exception) {
+            logger.warn("Unable to parse variables for feature \"" + key
+                    + "\". JsonParseException: " + exception);
+        }
 
         return new FeatureFlag(
                 id,
