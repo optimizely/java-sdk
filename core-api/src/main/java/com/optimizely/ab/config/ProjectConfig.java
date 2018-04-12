c828e8d267029e53c7d5e67aa7dafc356e1a1e65
wangjoshuah <wangjoshuah@gmail.com>
2018-04-12 13:45:04 -0700

    revise comments around throwing

--- core-api/src/main/java/com/optimizely/ab/config/ProjectConfig.java
+++ core-api/src/main/java/com/optimizely/ab/config/ProjectConfig.java
@@ -235,7 +235,7 @@ public class ProjectConfig {
                 getExperimentKeyMapping()
                         .get(experimentKey);
 
-        // if the given experiment key isn't present in the config, log and potentially throw an exception
+        // if the given experiment key isn't present in the config, log an exception to the error handler
         if (experiment == null) {
             String unknownExperimentError = String.format("Experiment \"%s\" is not in the datafile.", experimentKey);
             logger.error(unknownExperimentError);
@@ -261,7 +261,7 @@ public class ProjectConfig {
                 getEventNameMapping()
                         .get(eventName);
 
-        // if the given event name isn't present in the config, log and potentially throw an exception
+        // if the given event name isn't present in the config, log an exception to the error handler
         if (eventType == null) {
             String unknownEventTypeError = String.format("Event \"%s\" is not in the datafile.", eventName);
             logger.error(unknownEventTypeError);
