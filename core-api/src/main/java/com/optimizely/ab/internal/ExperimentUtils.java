1faa8b8af89bbb8f9bc64b3aaf46cfbb5b4a6014
Thomas Zurkan <thomas.zurkan@optimizely.com>
2018-10-08 22:01:38 -0700

    start using audienceConditions

--- core-api/src/main/java/com/optimizely/ab/internal/ExperimentUtils.java
+++ core-api/src/main/java/com/optimizely/ab/internal/ExperimentUtils.java
@@ -18,9 +18,12 @@ package com.optimizely.ab.internal;
 
 import com.optimizely.ab.config.Experiment;
 import com.optimizely.ab.config.ProjectConfig;
+import com.optimizely.ab.config.audience.AndCondition;
 import com.optimizely.ab.config.audience.Audience;
 import com.optimizely.ab.config.audience.AudienceHolderCondition;
 import com.optimizely.ab.config.audience.Condition;
+import com.optimizely.ab.config.audience.NotCondition;
+import com.optimizely.ab.config.audience.OrCondition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -61,6 +64,10 @@ public final class ExperimentUtils {
     public static boolean isUserInExperiment(@Nonnull ProjectConfig projectConfig,
                                              @Nonnull Experiment experiment,
                                              @Nonnull Map<String, ?> attributes) {
+        if (experiment.getAudienceConditions() != null) {
+            Boolean resolveReturn = resolveAudienceConditions(projectConfig, experiment, attributes);
+            return resolveReturn == null ? false : resolveReturn;
+        }
         List<String> experimentAudienceIds = experiment.getAudienceIds();
 
         // if there are no audiences, ALL users should be part of the experiment
@@ -79,4 +86,56 @@ public final class ExperimentUtils {
 
         return false;
     }
+
+    public static boolean resolveAudienceConditions(@Nonnull ProjectConfig projectConfig,
+                                            @Nonnull Experiment experiment,
+                                            @Nonnull Map<String, ?> attributes) {
+
+        resolveAudienceIdConditions(projectConfig, experiment.getAudienceConditions());
+
+        return experiment.getAudienceConditions().evaluate(attributes);
+    }
+
+    public static void resolveAudienceIdConditions(@Nonnull ProjectConfig projectConfig,
+                                                    @Nonnull Condition conditions) {
+
+        if (conditions instanceof AndCondition) {
+            AndCondition andCondition = (AndCondition) conditions;
+            for (Condition condition : andCondition.getConditions()) {
+                if (condition instanceof AudienceHolderCondition) {
+                    AudienceHolderCondition holder = (AudienceHolderCondition) condition;
+                    holder.setAudience(projectConfig.getAudience(holder.getAudienceId()));
+                }
+                else {
+                    resolveAudienceIdConditions(projectConfig, condition);
+                }
+            }
+        }
+        else if (conditions instanceof OrCondition) {
+            OrCondition orCondition = (OrCondition) conditions;
+            for (Condition condition : orCondition.getConditions()) {
+                if (condition instanceof AudienceHolderCondition) {
+                    AudienceHolderCondition holder = (AudienceHolderCondition) condition;
+                    holder.setAudience(projectConfig.getAudience(holder.getAudienceId()));
+                }
+                else {
+                    resolveAudienceIdConditions(projectConfig, condition);
+                }
+            }
+        }
+        else if (conditions instanceof NotCondition) {
+            NotCondition notCondition = (NotCondition) conditions;
+
+            Condition condition = notCondition.getCondition();
+            if (condition instanceof AudienceHolderCondition) {
+                AudienceHolderCondition holder = (AudienceHolderCondition) condition;
+                holder.setAudience(projectConfig.getAudience(holder.getAudienceId()));
+            }
+            else {
+                resolveAudienceIdConditions(projectConfig, conditions);
+            }
+        }
+
+    }
+
 }
