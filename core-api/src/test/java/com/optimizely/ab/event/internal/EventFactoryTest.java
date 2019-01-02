18d54d396d3915c7d9ced1f79ef6e8c5501389f3
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-01-02 15:19:06 +0500

    Updated headers

--- core-api/src/test/java/com/optimizely/ab/event/internal/EventFactoryTest.java
+++ core-api/src/test/java/com/optimizely/ab/event/internal/EventFactoryTest.java
@@ -1,6 +1,6 @@
 /**
  *
- *    Copyright 2016-2018, Optimizely and contributors
+ *    Copyright 2016-2019, Optimizely and contributors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -340,7 +340,7 @@ public class EventFactoryTest {
      * causing an exception to be thrown and passing only the valid attributes.
      */
     @Test
-    public void createConversionEventIgnoresInvalidAndAcceptsValidValueOfValidTypeAttributes() {
+    public void createConversionEventIgnoresInvalidAcceptValidValOfValidAttr() {
         assumeTrue(datafileVersion >= Integer.parseInt(ProjectConfig.Version.V4.toString()));
 
         Bucketer mockBucketAlgorithm = mock(Bucketer.class);
