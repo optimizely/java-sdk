4b6561d1d83dbc5f998a6ccc3021a79a3b61a6d7
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-01-22 22:32:15 -0800

    fix example by creating new user each time so that events can be done multiple times

--- java-quickstart/src/main/java/com/optimizely/Example.java
+++ java-quickstart/src/main/java/com/optimizely/Example.java
@@ -12,6 +12,8 @@ import org.apache.http.util.EntityUtils;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.util.Random;
+
 public class Example {
     public static void main(String[] args) {
 
@@ -20,12 +22,14 @@ public class Example {
         try {
             Optimizely optimizely = Optimizely.builder(datafile, new AsyncEventHandler(100,2)).build();
             System.out.println("hello world");
+            Random random = new Random();
+            String user = String.valueOf(random.nextInt());
             Map<String,String> attributes = null;//new HashMap<>();
             //attributes.put("browser_type", "crome");
-            Variation v = optimizely.activate("background_experiment", "testuserid", attributes);
+            Variation v = optimizely.activate("background_experiment", user, attributes);
             if (v != null) {
                 System.out.println(String.format("Found variation %s", v.getKey()));
-                optimizely.track("sample_conversion", "testuserid", attributes);
+                optimizely.track("sample_conversion", user, attributes);
                 Thread.sleep(10000);
             }
             else {
