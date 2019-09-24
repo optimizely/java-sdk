8946f4044081f730af8510c63ce0112871539c49
fayyaz arshad <fayyazarshad@folio3.com>
2019-09-24 16:01:17 +0500

    Added Bucketer

---
+++ core-api/src/main/java/com/optimizely/ab/decision/bucketer/Bucketer.java
@@ -0,0 +1,30 @@
+/****************************************************************************
+ * Copyright 2019, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+package com.optimizely.ab.decision.bucketer;
+
+/**
+ * Default Optimizely bucketing algorithm that evenly distributes users using the Murmur3 hash of some provided
+ * identifier.
+ */
+public interface Bucketer {
+    /**
+     * Map the given 32-bit hashcode into the range [0, MAX_TRAFFIC_VALUE]).
+     *
+     * @param hashCode the provided hashcode
+     * @return a value in the range closed-open range, [0, MAX_TRAFFIC_VALUE])
+     */
+    int generateBucketValue(int hashCode);
+}
