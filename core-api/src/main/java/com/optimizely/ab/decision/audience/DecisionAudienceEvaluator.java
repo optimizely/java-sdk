cb1bc4b54fa5e76010063c53e8af810f57be6d46
fayyaz arshad <fayyazarshad@folio3.com>
2019-09-25 11:05:16 +0500

    Experiment Utils logic is moved to the concrete Audience Evaluator

--- core-api/src/main/java/com/optimizely/ab/decision/audience/DecisionAudienceEvaluator.java
+++ core-api/src/main/java/com/optimizely/ab/decision/audience/DecisionAudienceEvaluator.java
@@ -17,28 +17,93 @@ package com.optimizely.ab.decision.audience;
 
 import com.optimizely.ab.config.Experiment;
 import com.optimizely.ab.config.ProjectConfig;
+import com.optimizely.ab.config.audience.AudienceIdCondition;
+import com.optimizely.ab.config.audience.Condition;
+import com.optimizely.ab.config.audience.OrCondition;
 import com.optimizely.ab.event.internal.UserContext;
-import com.optimizely.ab.internal.ExperimentUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Determines whether a user satisfies audience conditions for the experiment.
  */
 public class DecisionAudienceEvaluator implements AudienceEvaluator {
-    /**
-     * Validate Audience conditions for the experiment
+
+    private static final Logger logger = LoggerFactory.getLogger(DecisionAudienceEvaluator.class);
+
+       /**
+     * Validates user satisfies audience conditions for the experiment
      *
      * @param experiment  The Experiment the user will be bucketed into.
      * @param userContext It have user id, attributes and a reference to the current {@link ProjectConfig}
-     * @return flag if audience attributes are valid
+     * @return flag if user satisfies audience conditions for the experiment
      */
     @Override
     public boolean evaluate(@Nonnull Experiment experiment,
                             @Nonnull UserContext userContext) {
-        return ExperimentUtils.isUserInExperiment(
-            userContext.getProjectConfig(),
-            experiment,
-            userContext.getAttributes());
+        if (experiment.getAudienceConditions() != null) {
+            Boolean resolveReturn = evaluateAudienceConditions(experiment, userContext);
+            return resolveReturn == null ? false : resolveReturn;
+        } else {
+            Boolean resolveReturn = evaluateAudience(experiment, userContext);
+            return Boolean.TRUE.equals(resolveReturn);
+        }
+    }
+
+    /**
+     * Evaluate audience if experiment has no conditions
+     *
+     * @param experiment  The Experiment the user will be bucketed into.
+     * @param userContext It have user id, attributes and a reference to the current {@link ProjectConfig}
+     * @return flag if audience are valid for experiment
+     */
+    @Nullable
+    private static Boolean evaluateAudience(@Nonnull Experiment experiment,
+                                            @Nonnull UserContext userContext) {
+        List<String> experimentAudienceIds = experiment.getAudienceIds();
+        // if there are no audiences, ALL users should be part of the experiment
+        if (experimentAudienceIds.isEmpty()) {
+            logger.debug("There is no Audience associated with experiment {}", experiment.getKey());
+            return true;
+        }
+        List<Condition> conditions = new ArrayList<>();
+        for (String audienceId : experimentAudienceIds) {
+            AudienceIdCondition condition = new AudienceIdCondition(audienceId);
+            conditions.add(condition);
+        }
+        OrCondition implicitOr = new OrCondition(conditions);
+        logger.debug("Evaluating audiences for experiment \"{}\": \"{}\"", experiment.getKey(), conditions);
+
+        Boolean result = implicitOr.evaluate(userContext.getProjectConfig(), userContext.getAttributes());
+        logger.info("Audiences for experiment {} collectively evaluated to {}", experiment.getKey(), result);
+
+        return result;
+    }
+
+    /**
+     * Evaluate audience if experiment has conditions
+     *
+     * @param experiment  The Experiment the user will be bucketed into.
+     * @param userContext It have user id, attributes and a reference to the current {@link ProjectConfig}
+     * @return flag if audience conditions are valid for experiment
+     */
+    private static Boolean evaluateAudienceConditions(@Nonnull Experiment experiment,
+                                                      @Nonnull UserContext userContext) {
+        Condition conditions = experiment.getAudienceConditions();
+        if (conditions == null) return null;
+        logger.debug("Evaluating audiences for experiment \"{}\": \"{}\"", experiment.getKey(), conditions.toString());
+        try {
+            Boolean result = conditions.evaluate(userContext.getProjectConfig(), userContext.getAttributes());
+            logger.info("Audiences for experiment {} collectively evaluated to {}", experiment.getKey(), result);
+            return result;
+        } catch (Exception e) {
+            logger.error("Condition invalid", e);
+            return null;
+        }
     }
 }
