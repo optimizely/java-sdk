5bfbb60e8cfb7a7b2db4c17a3b39908826a3445e
fayyaz arshad <fayyaz.aarshad@gmail.com>
2019-12-10 22:15:58 +0500

    Changes after review

--- core-api/src/main/java/com/optimizely/ab/Optimizely.java
+++ core-api/src/main/java/com/optimizely/ab/Optimizely.java
@@ -457,19 +457,6 @@ public class Optimizely implements AutoCloseable {
     }
 
     /**
-     * Get {@link OptimizelyConfig} containing experiments and features map
-     *
-     * @return {@link OptimizelyConfig}
-     */
-    public OptimizelyConfig getOptimizelyConfig() {        
-        if (!isValid()) {
-            logger.error("Optimizely instance is not valid, failing getOptimizelyConfig call.");
-            return null;
-        }
-        return new OptimizelyConfigService().getOptimizelyConfig(getProjectConfig());
-    }
-
-    /**
      * Get the Double value of the specified variable in the feature.
      *
      * @param featureKey  The unique key of the feature.
@@ -908,6 +895,19 @@ public class Optimizely implements AutoCloseable {
     @Nullable
     public UserProfileService getUserProfileService() {
         return userProfileService;
+    }
+
+    /**
+     * Get {@link OptimizelyConfig} containing experiments and features map
+     *
+     * @return {@link OptimizelyConfig}
+     */
+    public OptimizelyConfig getOptimizelyConfig() {
+        if (!isValid()) {
+            logger.error("Optimizely instance is not valid, failing getOptimizelyConfig call.");
+            return null;
+        }
+        return new OptimizelyConfigService().getOptimizelyConfig(getProjectConfig());
     }
 
     //======== Helper methods ========//
