642f957534c11a74c68a37112b38bcdde9ba9c98
Jae Kim <jae.kim@optimizely.com>
2020-10-07 17:47:59 -0700

    fix errors

--- core-api/src/main/java/com/optimizely/ab/optimizelyusercontext/OptimizelyDecision.java
+++ core-api/src/main/java/com/optimizely/ab/optimizelyusercontext/OptimizelyDecision.java
@@ -1,9 +1,27 @@
+/**
+ *
+ *    Copyright 2020, Optimizely and contributors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.optimizely.ab.optimizelyusercontext;
 
 import com.optimizely.ab.optimizelyjson.OptimizelyJSON;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.List;
 
 public class OptimizelyDecision {
     @Nullable
@@ -11,7 +29,7 @@ public class OptimizelyDecision {
 
     private final boolean enabled;
 
-    @Nonnull
+    @Nullable
     private final OptimizelyJSON variables;
 
     @Nullable
@@ -24,16 +42,16 @@ public class OptimizelyDecision {
     private final OptimizelyUserContext userContext;
 
     @Nonnull
-    private String[] reasons;
+    private List<String> reasons;
 
 
     public OptimizelyDecision(@Nullable String variationKey,
                               boolean enabled,
-                              @Nonnull OptimizelyJSON variables,
+                              @Nullable OptimizelyJSON variables,
                               @Nullable String ruleKey,
                               @Nonnull String flagKey,
-                              @Nullable OptimizelyUserContext userContext,
-                              @Nonnull String[] reasons) {
+                              @Nonnull OptimizelyUserContext userContext,
+                              @Nonnull List<String> reasons) {
         this.variationKey = variationKey;
         this.enabled = enabled;
         this.variables = variables;
@@ -73,20 +91,21 @@ public class OptimizelyDecision {
     }
 
     @Nonnull
-    public String[] getReasons() {
+    public List<String> getReasons() {
         return reasons;
     }
 
     public static OptimizelyDecision createErrorDecision(@Nonnull String key,
                                                          @Nonnull OptimizelyUserContext user,
-                                                         String error) {
-        return new OptimizelyDecision(null,
+                                                         @Nonnull String error) {
+        return new OptimizelyDecision(
+            null,
             false,
             null,
             null,
             key,
             user,
-            new String[]{error});
+            Arrays.asList(error));
     }
 
     public boolean hasFailed() {
