c8cfb27334f0f1b522c652a1b292460dacb2e29c
Jae Kim <jae.kim@optimizely.com>
2021-01-07 17:15:29 -0800

    add comments

--- core-api/src/main/java/com/optimizely/ab/optimizelydecision/OptimizelyDecision.java
+++ core-api/src/main/java/com/optimizely/ab/optimizelydecision/OptimizelyDecision.java
@@ -26,23 +26,44 @@ import java.util.Collections;
 import java.util.List;
 
 public class OptimizelyDecision {
+    /**
+     * The variation key of the decision. This value will be null when decision making fails.
+     */
     @Nullable
     private final String variationKey;
 
+    /**
+     * The boolean value indicating if the flag is enabled or not.
+     */
     private final boolean enabled;
 
+    /**
+     * The collection of variables associated with the decision.
+     */
     @Nonnull
     private final OptimizelyJSON variables;
 
+    /**
+     * The rule key of the decision.
+     */
     @Nullable
     private final String ruleKey;
 
+    /**
+     * The flag key for which the decision has been made for.
+     */
     @Nonnull
     private final String flagKey;
 
+    /**
+     * A copy of the user context for which the decision has been made for.
+     */
     @Nonnull
     private final OptimizelyUserContext userContext;
 
+    /**
+     * An array of error/info messages describing why the decision has been made.
+     */
     @Nonnull
     private List<String> reasons;
 
