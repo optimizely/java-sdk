974ea931861d3913bb78a28c8d4600e222dad045
Jacob Brown <jacob.brown@optimizely.com>
2021-08-06 09:42:04 -0400

    remove redundant null check.

--- core-api/src/main/java/com/optimizely/ab/optimizelyconfig/OptimizelyConfig.java
+++ core-api/src/main/java/com/optimizely/ab/optimizelyconfig/OptimizelyConfig.java
@@ -50,8 +50,8 @@ public class OptimizelyConfig {
         this.experimentsMap = experimentsMap;
         this.featuresMap = featuresMap;
         this.revision = revision;
-        this.sdkKey = sdkKey.isEmpty() || sdkKey == null ? "" : sdkKey;
-        this.environmentKey = environmentKey.isEmpty() || environmentKey == null ? "" : environmentKey;
+        this.sdkKey = sdkKey.isEmpty() ? "" : sdkKey;
+        this.environmentKey = environmentKey.isEmpty() ? "" : environmentKey;
         this.attributes = attributes;
         this.events = events;
         this.audiences = audiences;
