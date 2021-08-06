ec15d257355f3bdf2f8a0b43221f8fa2205480c6
Jacob Brown <jacob.brown@optimizely.com>
2021-08-06 10:38:45 -0400

    revert to test FSC

--- core-api/src/main/java/com/optimizely/ab/optimizelyconfig/OptimizelyConfig.java
+++ core-api/src/main/java/com/optimizely/ab/optimizelyconfig/OptimizelyConfig.java
@@ -50,8 +50,8 @@ public class OptimizelyConfig {
         this.experimentsMap = experimentsMap;
         this.featuresMap = featuresMap;
         this.revision = revision;
-        this.sdkKey = sdkKey == null ? "" : sdkKey;
-        this.environmentKey = environmentKey == null ? "" : environmentKey;
+        this.sdkKey = sdkKey;
+        this.environmentKey = environmentKey;
         this.attributes = attributes;
         this.events = events;
         this.audiences = audiences;
