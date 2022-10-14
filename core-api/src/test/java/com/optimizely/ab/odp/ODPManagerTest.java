f493cbfc55ef49d4aa1598fd3a2d21536be9d63b
zashraf1985 <zeeshanashraf1985@gmail.com>
2022-10-14 01:18:41 -0700

    Added license headers.

--- core-api/src/test/java/com/optimizely/ab/odp/ODPManagerTest.java
+++ core-api/src/test/java/com/optimizely/ab/odp/ODPManagerTest.java
@@ -1,3 +1,19 @@
+/**
+ *
+ *    Copyright 2022, Optimizely
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.optimizely.ab.odp;
 
 import org.junit.Before;
@@ -63,7 +79,7 @@ public class ODPManagerTest {
 
         odpManager.updateSettings("test-host-updated", "test-key-updated", Arrays.asList("segment1"));
         odpManager.getEventManager().identifyUser("vuid", "fsuid");
-        Thread.sleep(2000);
+        Thread.sleep(1200);
         verify(mockApiManager, times(1))
             .sendEvents(eq("test-key-updated"), eq("test-host-updated/v3/events"), any());
     }
